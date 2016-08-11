94d11841c7bf519b863618a80d3522c7c3dfba9b
asutedja <asutedja@gmail.com>
2016-08-11 15:18:30 -0700

    Finish min requirements

--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -6,7 +6,7 @@ var LibraryView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
-    this.collection.on('', this.render, this);
+    this.collection.on('sync', this.render, this);
   },
 
   render: function() {
